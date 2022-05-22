796181ce6d2dc63740e1933d6ba5cde906d5c10f
RoninQA <ronierisoncosta@gmail.com>
2022-05-22 14:47:11 -0300

    Criando a classe DateUtils para deixar o código mais limpo.

--- src/main/java/servicos/AluguelService.java
+++ src/main/java/servicos/AluguelService.java
@@ -2,6 +2,7 @@ package servicos;
 
 import entidades.Filme;
 import entidades.NotaAluguel;
+import utils.DateUtils;
 
 import java.util.Calendar;
 
@@ -9,11 +10,10 @@ public class AluguelService {
     public NotaAluguel alugar(Filme filme) {
         if (filme.getEstoque() == 0)
             throw new RuntimeException("Filme sem estoque");
+
         NotaAluguel nota = new NotaAluguel();
         nota.setPreco(filme.getAluguel());
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_MONTH, 1);
-        nota.setDataEntrega(cal.getTime());
+        nota.setDataEntrega(DateUtils.obterDataDiferencaDias(1));
         filme.setEstoque(filme.getEstoque() - 1);
         return nota;
     }
