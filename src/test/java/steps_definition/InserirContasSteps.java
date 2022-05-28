55b9878bd0fd62fda6f15dc912891e760cfb9c19
RoninQA <ronierisoncosta@gmail.com>
2022-05-28 10:58:24 -0300

    Mudando as formas de descrição do cenário de Imperativo para Declarativo deixando o cenário mais enxuto.

--- src/test/java/steps_definition/InserirContasSteps.java
+++ src/test/java/steps_definition/InserirContasSteps.java
@@ -20,69 +20,23 @@ import java.io.IOException;
 public class InserirContasSteps {
 
     private WebDriver driver;
-    @Dado("^que estou acessando a aplicação$")
-    public void que_estou_acessando_a_aplicação() throws Throwable {
+
+    @Dado("^que desejo adicionar uma conta$")
+    public void que_desejo_adicionar_uma_conta() throws Throwable {
         driver = new ChromeDriver();
         driver.get("https://seubarriga.wcaquino.me/");
-    }
-
-    @Quando("^informo o usuário \"([^\"]*)\"$")
-    public void informo_o_usuário(String arg1) throws Throwable {
-        driver.findElement(By.id("email")).sendKeys(arg1);
-    }
-
-    @Quando("^a senha \"([^\"]*)\"$")
-    public void a_senha(String arg1) throws Throwable {
-        driver.findElement(By.name("senha")).sendKeys(arg1);
-    }
-
-    @Quando("^seleciono entrar$")
-    public void seleciono_entrar() throws Throwable {
+        driver.findElement(By.id("email")).sendKeys("ronierisoncosta@gmail.com");
+        driver.findElement(By.name("senha")).sendKeys("zeca21121991");
         driver.findElement(By.tagName("button")).click();
-    }
-
-    @Então("^visualizo a página inicial$")
-    public void visualizo_a_página_inicial() throws Throwable {
-       String texto = driver.findElement(By.xpath("//div[@class='alert alert-success']")).getText();
-        Assert.assertEquals("Bem vindo, RONIERISON MOREIRA DA COSTA!", texto);
-    }
-
-    @Quando("^seleciono Contas$")
-    public void seleciono_Contas() throws Throwable {
         driver.findElement(By.linkText("Contas")).click();
-    }
-
-    @Quando("^seleciono Adicionar$")
-    public void seleciono_Adicionar() throws Throwable {
         driver.findElement(By.linkText("Adicionar")).click();
+
     }
 
-    @Quando("^informo a conta \"([^\"]*)\"$")
-    public void informo_a_conta(String arg1) throws Throwable {
+    @Quando("^adiciono a conta \"([^\"]*)\"$")
+    public void adiciono_a_conta(String arg1) throws Throwable {
         driver.findElement(By.id("nome")).sendKeys(arg1);
-    }
-
-    @Quando("^seleciono Salvar$")
-    public void seleciono_Salvar() throws Throwable {
         driver.findElement(By.tagName("button")).click();
-    }
-
-    @Então("^a conta é inserida com sucesso$")
-    public void a_conta_é_inserida_com_sucesso() throws Throwable {
-        String texto = driver.findElement(By.xpath("//div[@class='alert alert-success']")).getText();
-        Assert.assertEquals("Conta adicionada com sucesso!", texto);
-    }
-
-    @Então("^sou notificado que o nome da conta é obrigatório$")
-    public void sou_notificar_que_o_nome_da_conta_é_obrigatório() throws Throwable {
-        String texto = driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText();
-        Assert.assertEquals("Informe o nome da conta", texto);
-    }
-
-    @Então("^sou notificado que já existe uma conta com esse nome$")
-    public void sou_notificado_que_já_existe_uma_conta_com_esse_nome() throws Throwable {
-        String texto = driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText();
-        Assert.assertEquals("Já existe uma conta com esse nome!", texto);
     }
 
     @Então("^recebo a mensagem \"([^\"]*)\"$")
@@ -90,10 +44,7 @@ public class InserirContasSteps {
         String texto = driver.findElement(By.xpath("//div[starts-with(@class, 'alert alert-')]")).getText();
         Assert.assertEquals(arg1, texto);
     }
-    @Before
-    public void inicio() {
-        System.out.println("Começando aqui");
-    }
+
     @After (order = 1, value = {"@funcionais"})
     public void screenshot(Scenario cenario) {
         File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
@@ -107,6 +58,5 @@ public class InserirContasSteps {
     @After (order = 0, value = {"@funcionais"})
     public  void fecharBrowser() {
         driver.quit();
-        System.out.println("Terminando aqui");
     }
 }
